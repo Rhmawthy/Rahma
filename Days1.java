c97c1a0636ad7f585cb7850961e9b7d96d4efc1e
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2024-09-02 17:18:11 +0800

    Add files via upload

---
+++ Days1.java
@@ -0,0 +1,11 @@
+
+package DaysOfCoding;
+
+
+public class Days1 {
+    public static void main(String[] args) {
+        System.out.println("rahma");
+        System.out.println("rahma");
+    }
+    
+}
